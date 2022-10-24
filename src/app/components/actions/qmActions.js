ec3c8363917f7851fccb4b0a169b0ddd5e0be10b
ketnek <kette92@gmail.com>
2022-10-24 05:27:34 +0200

    Finish quote generator

--- src/app/components/actions/qmActions.js
+++ src/app/components/actions/qmActions.js
@@ -1,4 +1,9 @@
 export const NEW = 'NEW';
+export const NEW_COLOR = 'NEW_COLOR';
 
 export const newQuote = () => ({ type: NEW, });
+export const newColor = (color) => ({
+  type: NEW_COLOR,
+  newColor: color
+});
 
